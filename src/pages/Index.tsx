14c8d6718e8a7961a60e7f80c6e4aeb0dde5c1ed
Hemanth Vignesh V <142784687+HemanthVignesh@users.noreply.github.com>
2026-03-23 11:22:03 +0530

    portfolio update

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -23,10 +23,10 @@ const Index = () => (
       <HeroSection />
       <ImpactMetrics />
       <AboutSection />
+      <SkillsSection />
       <ProjectsSection />
       <ExperienceSection />
       <AchievementsSection />
-      <SkillsSection />
       <ResumeCTA />
       <ContactSection />
       <Footer />
